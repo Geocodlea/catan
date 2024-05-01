469bc4df07ee7dcb28aaa2b41aa653e14ac57eb4
Geocodlea <geocodlea@yahoo.com>
2024-05-01 19:51:54 +0300

    test rich text editor

--- app/events/[...id]/page.js
+++ app/events/[...id]/page.js
@@ -3,22 +3,32 @@ import Event from "/models/Event";
 
 import Tabs from "@/components/Tabs";
 
+import dynamic from "next/dynamic";
+const Test = dynamic(() => import("./Test"), { ssr: false });
+
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth/next";
+
 export default async function EventPage({ params }) {
+  const session = await getServerSession(authOptions);
+  const isAdmin = session?.user.role === "admin";
+
   await dbConnect();
   const events = await Event.find({ _id: params.id }).select("detalii");
 
   const textFromMongoDB = events[0].detalii;
 
-  const detalii = <p dangerouslySetInnerHTML={{ __html: textFromMongoDB }} />;
-
   return (
-    <Tabs
-      tabContents={[
-        {
-          label: "Detalii",
-          content: detalii,
-        },
-      ]}
-    />
+    <>
+      {/* <Tabs
+        tabContents={[
+          {
+            label: "Detalii",
+            content: detalii,
+          },
+        ]}
+      /> */}
+      {isAdmin && <Test />}
+    </>
   );
 }
