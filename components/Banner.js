e4cbaf8132246693094efae8a8f23b5ff5894261
Geocodlea <geocodlea@yahoo.com>
2024-06-11 16:19:20 +0300

    change banner images

--- components/Banner.js
+++ components/Banner.js
@@ -2,31 +2,56 @@
 
 import { Box } from "@mui/material";
 
-import { useState, useEffect } from "react";
-import { useSelectedLayoutSegment } from "next/navigation";
+import { useSelectedLayoutSegments } from "next/navigation";
 import Image from "next/image";
 import bg from "/public/img/bg.jpg";
+import catanBg from "/public/img/catan_bg.webp";
+import cavaleriBg from "/public/img/cavaleri_bg.webp";
+import rentzBg from "/public/img/rentz_bg.webp";
 import aboutBg from "/public/img/about_bg.jpg";
 import contactBg from "/public/img/contact_bg.jpg";
 import profileBg from "/public/img/profile_bg.jpg";
-// import otherBg from "/public/img/other_bg.jpg";
 
 function Banner({ children }) {
-  const segment = useSelectedLayoutSegment();
-
+  const segments = useSelectedLayoutSegments();
   let imageUrl;
-  switch (segment) {
-    case "about":
-      imageUrl = aboutBg;
-      break;
-    case "contact":
-      imageUrl = contactBg;
-      break;
-    case "profile":
-      imageUrl = profileBg;
-      break;
-    default:
-      imageUrl = bg;
+
+  const [segment, secondSegment] = segments;
+
+  if (segments.length === 0) {
+    imageUrl = catanBg;
+  } else if (segments.length === 1) {
+    switch (segment) {
+      case "about":
+        imageUrl = aboutBg;
+        break;
+      case "contact":
+        imageUrl = contactBg;
+        break;
+      case "profile":
+        imageUrl = profileBg;
+        break;
+      default:
+        imageUrl = bg;
+    }
+  } else if (segments[0] === "events") {
+    const eventType = secondSegment.split("/")[0];
+    switch (eventType) {
+      case "catan":
+        imageUrl = catanBg;
+        break;
+      case "cavaleri":
+        imageUrl = cavaleriBg;
+        break;
+      case "whist":
+      case "rentz":
+        imageUrl = rentzBg;
+        break;
+      default:
+        imageUrl = catanBg;
+    }
+  } else {
+    imageUrl = bg;
   }
 
   return (
