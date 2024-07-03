fefaba71f60909806b5ba9e08c7079f375f8dc0e
Geocodlea <geocodlea@yahoo.com>
2024-07-03 14:39:20 +0300

    reset dynamic in progress

--- app/api/events/reset/[...type]/route.js
+++ app/api/events/reset/[...type]/route.js
@@ -1,11 +1,15 @@
 import dbConnect from "/utils/dbConnect";
 import { NextResponse } from "next/server";
-import * as Participants from "@/models/Participants";
-import * as Verifications from "@/models/Verifications";
-import * as Matches from "@/models/Matches";
-import * as Clasament from "@/models/Clasament";
 import OldEvents from "@/models/OldEvents";
 import Event from "@/models/Event";
+
+import mongoose from "mongoose";
+import {
+  createParticipantsModel,
+  createVerificationsModel,
+  createMatchesModel,
+  createClasamentModel,
+} from "@/utils/createModels";
 
 import { sortOrder } from "@/utils/helpers";
 
@@ -25,12 +29,12 @@ const bucketName = process.env.GOOGLE_CLOUD_BUCKET_NAME;
 const bucket = storage.bucket(bucketName);
 
 // Function to drop Matches based on type
-const dropMatches = async (type) => {
+const dropMatches = async (eventID) => {
   // Get all keys from the Matches object
   const allKeys = Object.keys(Matches);
 
   // Filter keys based on the specified type
-  const filteredKeys = allKeys.filter((key) => key.includes(type));
+  const filteredKeys = allKeys.filter((key) => key.includes(eventID));
 
   // Drop each filtered collection
   for (const key of filteredKeys) {
@@ -41,77 +45,84 @@ const dropMatches = async (type) => {
 export async function DELETE(request, { params }) {
   const [type, round, isFinalRound, eventID] = params.type;
 
-  const ParticipantType = Participants[`Participanti_live_${type}`];
-  const VerificationsType = Verifications[`Verificari_live_${type}`];
-  const MatchesType = Matches[`Meciuri_live_${type}_${round}`];
-  const ClasamentType = Clasament[`Clasament_live_${type}`];
-
+  // Create models
   await dbConnect();
+  await createParticipantsModel(eventID);
+  await createVerificationsModel(eventID);
+  await createMatchesModel(eventID);
+  await createClasamentModel(eventID);
+  const Participants = mongoose.models[`Participanti_live_${eventID}`];
+  const Verifications = mongoose.models[`Verificari_live_${eventID}`];
+  const Matches = mongoose.models[`Meciuri_live_${eventID}_${round}`];
+  const Clasament = mongoose.models[`Clasament_live_${eventID}`];
 
-  if (isFinalRound === "true") {
-    const roundScores = await MatchesType.find({
-      score: null,
-    }).count();
-    const allScoresSubmitted = roundScores === 0;
-    if (!allScoresSubmitted) {
-      return NextResponse.json({
-        success: false,
-        message: "Nu sunt introduse toate scorurile",
-      });
-    }
-    const participants = await ClasamentType.find().sort(sortOrder(type));
-    // Create old event
-    const date = new Date();
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    const oldEventName = `clasament_live_${type}_${day}.${month}.${year}`;
-    const oldEvent = {
-      name: oldEventName,
-      data: participants.map((participant) => ({
-        id: participant.id,
-        nume: participant.name,
-        masar1: participant.masar1 || null,
-        masar2: participant.masar2 || null,
-        masar3: participant.masar3 || null,
-        puncter1: participant.puncter1 || null,
-        puncter2: participant.puncter2 || null,
-        puncter3: participant.puncter3 || null,
-        punctetotal: participant.punctetotal || null,
-        punctejocr1: participant.scorjocr1 || null,
-        punctejoctotalr1: participant.scortotalr1 || null,
-        punctejocr2: participant.scorjocr2 || null,
-        punctejoctotalr2: participant.scortotalr2 || null,
-        punctejocr3: participant.scorjocr3 || null,
-        punctejoctotalr3: participant.scortotalr3 || null,
-        punctejoctotal: participant.scorjocuri || null,
-        punctejocuri: participant.scortotal || null,
-        procent: participant.procent || null,
-        licitari: participant.licitari || null,
-      })),
-    };
-    await OldEvents.create(oldEvent);
-    await ClasamentType.collection.drop();
-    await dropMatches(type);
-    // List all files for deleted event
-    const [files] = await bucket.getFiles({
-      prefix: `uploads/events/${eventID}/`,
-    });
-    // Delete each file
-    await Promise.all(
-      files.map(async (file) => {
-        await file.delete();
-      })
-    );
-    await Event.deleteOne({ _id: eventID });
-  }
+  console.log(Matches);
+  await dropMatches(eventID);
 
-  await ParticipantType.deleteMany();
-  await VerificationsType.deleteMany({ round: { $exists: false } });
-  await VerificationsType.updateOne(
-    { round: { $exists: true } },
-    { round: 0, stop: false }
-  );
+  // if (isFinalRound === "true") {
+  //   const roundScores = await Matches.find({
+  //     score: null,
+  //   }).count();
+  //   const allScoresSubmitted = roundScores === 0;
+  //   if (!allScoresSubmitted) {
+  //     return NextResponse.json({
+  //       success: false,
+  //       message: "Nu sunt introduse toate scorurile",
+  //     });
+  //   }
+  //   const participants = await Clasament.find().sort(sortOrder(type));
+  //   // Create old event
+  //   const date = new Date();
+  //   const day = String(date.getDate()).padStart(2, "0");
+  //   const month = String(date.getMonth() + 1).padStart(2, "0");
+  //   const year = date.getFullYear();
+  //   const oldEventName = `clasament_live_${type}_${day}.${month}.${year}`;
+  //   const oldEvent = {
+  //     name: oldEventName,
+  //     data: participants.map((participant) => ({
+  //       id: participant.id,
+  //       nume: participant.name,
+  //       masar1: participant.masar1 || null,
+  //       masar2: participant.masar2 || null,
+  //       masar3: participant.masar3 || null,
+  //       puncter1: participant.puncter1 || null,
+  //       puncter2: participant.puncter2 || null,
+  //       puncter3: participant.puncter3 || null,
+  //       punctetotal: participant.punctetotal || null,
+  //       punctejocr1: participant.scorjocr1 || null,
+  //       punctejoctotalr1: participant.scortotalr1 || null,
+  //       punctejocr2: participant.scorjocr2 || null,
+  //       punctejoctotalr2: participant.scortotalr2 || null,
+  //       punctejocr3: participant.scorjocr3 || null,
+  //       punctejoctotalr3: participant.scortotalr3 || null,
+  //       punctejoctotal: participant.scorjocuri || null,
+  //       punctejocuri: participant.scortotal || null,
+  //       procent: participant.procent || null,
+  //       licitari: participant.licitari || null,
+  //     })),
+  //   };
+  //   await OldEvents.create(oldEvent);
+  //   await Clasament.collection.drop();
+  //   await dropMatches(eventID);
+  //   // List all files for deleted event
+  //   const [files] = await bucket.getFiles({
+  //     prefix: `uploads/events/${eventID}/`,
+  //   });
+  //   // Delete each file
+  //   await Promise.all(
+  //     files.map(async (file) => {
+  //       await file.delete();
+  //     })
+  //   );
+  //   await Event.deleteOne({ _id: eventID });
+  // }
+
+  // await Participants.deleteMany();
+  // await Verifications.deleteMany({ round: { $exists: false } });
+  // await Verifications.updateOne(
+  //   { round: { $exists: true } },
+  //   { round: 0, stop: false }
+  // );
 
   return NextResponse.json({ success: true });
 }
