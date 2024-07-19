33bc9b2bbe9c1b78262993887ae44670222c93c8
Geocodlea <geocodlea@yahoo.com>
2024-07-19 08:25:49 +0300

    small design fix

--- app/error.js
+++ app/error.js
@@ -1,16 +1,14 @@
 "use client";
 
-import styles from "./page.module.css";
-
 import { Typography } from "@mui/material";
 
 export default function ErrorPage() {
   return (
     <>
-      <Typography variant="h1" className={styles.title} color={"error"}>
+      <Typography variant="h1" color={"error"}>
         ERROR
       </Typography>
-      <Typography sx={{ fontSize: "2rem" }}>Something went wrong</Typography>
+      <Typography variant="h4">Something went wrong</Typography>
     </>
   );
 }
