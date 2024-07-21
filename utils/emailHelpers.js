6d3b1d031ac994e3d3857c5c6249861a0ead6ce1
Geocodlea <geocodlea@yahoo.com>
2024-07-21 14:43:01 +0300

    change email footer

--- utils/emailHelpers.js
+++ utils/emailHelpers.js
@@ -28,17 +28,12 @@ const transporter = nodemailer.createTransport({
 const token = (email) => jwt.sign({ email }, process.env.NEXTAUTH_SECRET);
 
 const footerText = (email) =>
-  `\n\n Ioana Mocanu \n Creative Board Gaming \n\n e: ioana.mocanu@cbgshop.ro \n t: 0736465213 \n w: www.cbgshop.ro / www.agames.ro \n f: www.facebook.com/CreativeBoardGaming \n\n If you no longer wish to receive emails from us, please unsubscribe, using this link: ${
+  `\n\n Ideal Board Games \n\n If you no longer wish to receive emails from us, please unsubscribe, using this link: ${
     process.env.DEPLOYED_URL
   }/api/unsubscribe/${token(email)} \n`;
 
 const footerHtml = (email) => `<p>
-  Ioana Mocanu<br>
-  Creative Board Gaming<br><br>
-  e: <a href="mailto:ioana.mocanu@cbgshop.ro">ioana.mocanu@cbgshop.ro</a><br>
-  t: <a href="tel:0736465213">0736465213</a><br>
-  w: <a href="http://www.cbgshop.ro">www.cbgshop.ro</a> / <a href="http://www.agames.ro">www.agames.ro</a><br>
-  f: <a href="http://www.facebook.com/CreativeBoardGaming">www.facebook.com/CreativeBoardGaming</a>
+  Ideal Board Games
 </p>
 <p>
   If you no longer wish to receive emails from us, please <a href="${
